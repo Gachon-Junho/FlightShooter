01593eaa0581ad0d44cee563839e120a4868c7a6
Junho <ojh0050118@gmail.com>
2024-11-04 18:22:33 +0900

    Update EnemyAircraft.cs

--- Assets/Scripts/Object/Aircrafts/Enemy/EnemyAircraft.cs
+++ Assets/Scripts/Object/Aircrafts/Enemy/EnemyAircraft.cs
@@ -5,6 +5,7 @@ public class EnemyAircraft : AttackableAircraft, IFollowingObject
     public override string Name { get; }
     public override double Speed { get; }
     public override double ShootInterval { get; }  
+    public override int HP { get; }
     
     public void FollowTo(GameObject obj, double speed)
     {
