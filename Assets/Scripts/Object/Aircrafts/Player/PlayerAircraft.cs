579334416c6f17521776550ecfcfdd7186adc4dc
Junho <ojh0050118@gmail.com>
2024-11-11 23:24:07 +0900

    Bind `InputSystem` event to script

--- Assets/Scripts/Object/Aircrafts/Player/PlayerAircraft.cs
+++ Assets/Scripts/Object/Aircrafts/Player/PlayerAircraft.cs
@@ -1,6 +1,13 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerAircraft : AttackableAircraft
 {
+    public void OnMove(InputAction.CallbackContext context)
+    {
+    }
     
+    public void OnFire(InputAction.CallbackContext context)
+    {
+    }
 }
